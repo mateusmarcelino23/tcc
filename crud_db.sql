cfa861b6bae60853193ccc2c082873a71ec2c4e2
Mateus <mateusmarcelino1023@gmail.com>
2025-05-30 12:40:43 -0300

    Novo db

--- crud_db.sql
+++ crud_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25/05/2025 às 19:31
+-- Tempo de geração: 30/05/2025 às 17:35
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -64,7 +64,7 @@ CREATE TABLE `anotacoes` (
 --
 
 INSERT INTO `anotacoes` (`id`, `id_professor`, `texto`, `data`) VALUES
-(50, 20, 'Olá', '2025-05-25 19:31:07');
+(56, 11, 'bom dia lindo dia', '2025-05-29 03:24:07');
 
 -- --------------------------------------------------------
 
@@ -91,45 +91,49 @@ INSERT INTO `emprestimo` (`id`, `id_aluno`, `id_professor`, `id_livro`, `data_em
 (282, 33, 20, 26, '2025-05-01', '2025-05-10', 0),
 (283, 34, 20, 29, '2025-05-02', '2025-05-11', 0),
 (284, 35, 20, 26, '2025-05-03', '2025-05-12', 0),
-(285, 36, 20, 29, '2025-05-04', '2025-05-13', 2),
+(285, 36, 20, 29, '2025-05-04', '2025-05-13', 1),
 (286, 34, 20, 26, '2025-05-05', '2025-05-22', 0),
-(287, 77, 20, 29, '2025-05-06', '2025-05-15', 0),
+(287, 77, 20, 29, '2025-05-06', '2025-05-15', 1),
 (288, 33, 20, 26, '2025-05-07', '2025-05-16', 0),
-(289, 34, 20, 29, '2025-05-08', '2025-05-17', 2),
+(289, 34, 20, 29, '2025-05-08', '2025-05-17', 1),
 (290, 35, 20, 26, '2025-05-09', '2025-05-18', 0),
 (291, 36, 20, 29, '2025-05-10', '2025-05-19', 0),
-(292, 37, 20, 26, '2025-05-11', '2025-05-20', 2),
-(293, 77, 20, 29, '2025-05-12', '2025-05-21', 0),
-(294, 33, 20, 26, '2025-05-13', '2025-05-22', 2),
+(292, 37, 20, 26, '2025-05-11', '2025-05-20', 1),
+(293, 77, 20, 29, '2025-05-12', '2025-05-21', 1),
+(294, 33, 20, 26, '2025-05-13', '2025-05-22', 1),
 (295, 34, 20, 29, '2025-05-14', '2025-05-23', 0),
 (296, 35, 20, 26, '2025-05-15', '2025-05-24', 0),
 (297, 36, 20, 29, '2025-05-16', '2025-05-25', 0),
-(298, 37, 20, 26, '2025-05-17', '2025-05-26', 2),
+(298, 37, 20, 26, '2025-05-17', '2025-05-26', 1),
 (299, 77, 20, 29, '2025-05-18', '2025-05-27', 0),
-(300, 33, 20, 26, '2025-05-19', '2025-05-28', 2),
+(300, 33, 20, 26, '2025-05-19', '2025-05-28', 1),
 (301, 34, 20, 29, '2025-05-20', '2025-05-29', 0),
 (302, 35, 20, 26, '2025-05-21', '2025-05-30', 0),
 (303, 36, 20, 29, '2025-05-22', '2025-05-31', 0),
 (304, 37, 20, 26, '2025-05-23', '2025-06-01', 0),
 (305, 34, 20, 29, '2025-05-24', '2025-06-02', 0),
-(306, 33, 20, 26, '2025-05-25', '2025-06-03', 2),
+(306, 33, 20, 26, '2025-05-25', '2025-06-03', 0),
 (307, 34, 20, 29, '2025-05-26', '2025-06-04', 0),
 (308, 35, 20, 26, '2025-05-27', '2025-06-05', 0),
 (309, 36, 20, 29, '2025-05-28', '2025-06-06', 0),
-(310, 37, 20, 26, '2025-05-29', '2025-06-07', 2),
-(311, 77, 20, 29, '2025-05-30', '2025-06-08', 0),
-(312, 33, 20, 26, '2025-05-31', '2025-06-09', 2),
+(310, 37, 20, 26, '2025-05-29', '2025-06-07', 0),
+(311, 34, 20, 26, '2025-05-30', '2025-06-08', 0),
+(312, 33, 20, 26, '2025-05-31', '2025-06-09', 0),
 (313, 34, 20, 29, '2025-06-01', '2025-06-10', 0),
-(314, 35, 20, 26, '2025-06-02', '2025-06-11', 2),
+(314, 35, 20, 26, '2025-06-02', '2025-06-11', 0),
 (315, 36, 20, 29, '2025-06-03', '2025-06-12', 0),
 (316, 37, 20, 26, '2025-06-04', '2025-06-13', 0),
 (317, 77, 20, 29, '2025-06-05', '2025-06-14', 0),
-(318, 33, 20, 26, '2025-06-06', '2025-06-15', 2),
+(318, 33, 20, 26, '2025-06-06', '2025-06-15', 0),
 (319, 34, 20, 29, '2025-06-07', '2025-06-16', 0),
 (320, 35, 20, 26, '2025-06-08', '2025-06-17', 0),
-(321, 36, 20, 29, '2025-06-09', '2025-06-18', 2),
+(321, 36, 20, 29, '2025-06-09', '2025-06-18', 0),
 (322, 34, 11, 26, '2025-05-24', '2025-06-07', 0),
-(323, 33, 20, 90, '2025-05-24', '2025-05-25', 0);
+(323, 33, 20, 90, '2025-05-24', '2025-05-25', 0),
+(324, 33, 19, 97, '2025-05-27', '2025-05-31', 0),
+(325, 33, 20, 102, '2025-05-28', '2025-05-31', 0),
+(326, 33, 11, 104, '2025-05-29', '2025-06-07', 0),
+(327, 33, 11, 106, '2025-05-30', '2025-08-15', 0);
 
 -- --------------------------------------------------------
 
@@ -139,8 +143,8 @@ INSERT INTO `emprestimo` (`id`, `id_aluno`, `id_professor`, `id_livro`, `data_em
 
 CREATE TABLE `livro` (
   `id` int(11) NOT NULL,
-  `nome_livro` varchar(40) NOT NULL,
-  `nome_autor` varchar(40) NOT NULL,
+  `nome_livro` varchar(190) NOT NULL,
+  `nome_autor` varchar(130) NOT NULL,
   `isbn` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -154,7 +158,21 @@ INSERT INTO `livro` (`id`, `nome_livro`, `nome_autor`, `isbn`) VALUES
 (88, 'O Pequeno Príncipe', 'Antoine De  Saint-exupéry', '9781539839897'),
 (89, 'História geral da civilização brasileira', 'Sérgio Buarque de Holanda', 'UVA:X001023965'),
 (90, 'Harry Potter e a Pedra Filosofal', 'J.K. Rowling', '9781781103685'),
-(91, 'Uma Vez Desbravador, Sempre Desbravador', 'Maksym Krupskyi', '9788534534864');
+(91, 'Uma Vez Desbravador, Sempre Desbravador', 'Maksym Krupskyi', '9788534534864'),
+(92, 'Meio ambiente – E eu com isso?', 'Nurit Bensusan', '9788575966426'),
+(94, 'Homem-Aranha: Aranhaverso', 'Dan Slott', '9786525909400'),
+(95, 'O Paladar não Retrocede', 'Carlos Ferreirinha', '9788582892282'),
+(96, 'Consagrado no Gramado', 'Roberto Assaf', '9786500380491'),
+(97, 'Homem-Aranha: História de Vida', 'Chip Zdarsky', '9786555128741'),
+(98, 'Espetacular Homem-Aranha: Corrente', 'Jonathan Hickman, Gerry Duggan', '9786559608492'),
+(99, 'Ultimate Homem-Aranha', 'Brian Michael Bendis', '9786525901121'),
+(100, 'Hotel Magnifique', 'Emily J. Taylor', '9786559814350'),
+(101, 'Coleção Histórica Marvel: O Homem-Aranha', 'Stan Lee', '9786555120608'),
+(102, 'ONLY YOU', 'Rosa Kane', ''),
+(103, 'Ian não está mais aqui', 'Luiz Antonio Aguiar', '9788506080238'),
+(104, 'O colecionador de lágrimas', 'Augusto Cury', '8576658089'),
+(105, 'Estudar, Eu Tô Ligado!', 'Fernando Targino Da Cunha', 'PKEY:CLDEAU6915'),
+(106, 'Às Suas Ordens: Usando o Poder do “Eu Sou”', 'Neville Goddard', '9788822828569');
 
 -- --------------------------------------------------------
 
@@ -177,9 +195,10 @@ CREATE TABLE `professor` (
 --
 
 INSERT INTO `professor` (`id`, `nome`, `cpf`, `email`, `senha`, `token_recuperacao`, `token_expiracao`) VALUES
-(11, 'João', '00000000000', 'joaozinho@gmail.com', '$2y$10$9wsKRk73Ak7JUVY88kKfM.fXP1c5t9aMP/o2J3IxJ/AsaVrCEpjZq', 'b1b2ed89cbb0c3a9b06d59fab56aaea0', '2025-05-25 20:16:13'),
+(11, 'João', '00000000000', 'joaozinho@gmail.com', '$2y$10$9wsKRk73Ak7JUVY88kKfM.fXP1c5t9aMP/o2J3IxJ/AsaVrCEpjZq', 'f2b963317e507895ac80d711bff85641', '2025-05-29 14:00:34'),
 (19, 'Fernando', '11111111111', 'umavezflamengo@gmail.com', '$2y$10$TIIcdDCgLOD.GUhE3AIc4eQE/0LIdsP/UQBb54zAVruMoh1AW8L.m', NULL, NULL),
-(20, 'Gabriel', '22222222222', 'gabrielhistoria@gmail.com', '$2y$10$2r4IOHUmBrhQK6cLTReere6B4jbvqf2O4.ERO75MLL7HTFY3kug2q', NULL, NULL);
+(20, 'Gabriel', '22222222222', 'gabrielhistoria@gmail.com', '$2y$10$2r4IOHUmBrhQK6cLTReere6B4jbvqf2O4.ERO75MLL7HTFY3kug2q', NULL, NULL),
+(21, 'Mateus', '99999999999', 'mateusmarcelino1023@gmail.com', '$2y$10$omg9HaFXflrenC5kPWNQSu/yqXElxBdIGPsibJ7GYlGZUpZutKpL.', NULL, NULL);
 
 --
 -- Índices para tabelas despejadas
@@ -233,25 +252,25 @@ ALTER TABLE `aluno`
 -- AUTO_INCREMENT de tabela `anotacoes`
 --
 ALTER TABLE `anotacoes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT de tabela `emprestimo`
 --
 ALTER TABLE `emprestimo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=324;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=328;
 
 --
 -- AUTO_INCREMENT de tabela `livro`
 --
 ALTER TABLE `livro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
 
 --
 -- AUTO_INCREMENT de tabela `professor`
 --
 ALTER TABLE `professor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Restrições para tabelas despejadas

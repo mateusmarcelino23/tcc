72a73ec142fa64bafb4c0a034127deb3753b8a48
Mateus <mateusmarcelino1023@gmail.com>
2025-06-02 18:00:42 -0300

    relatorios separado

--- trabalho_crud/frontend/relatorios_front.php
+++ trabalho_crud/frontend/relatorios_front.php
@@ -9,6 +9,9 @@ include '../backend/relatorios.php';
   <meta charset="UTF-8">
   <title>Relatórios</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+  <link rel="stylesheet" type="text/css" href="../estilos/style.css">
   <link rel="stylesheet" type="text/css" href="../estilos/relatorios.css">
   <script src="https://www.gstatic.com/charts/loader.js"></script>
 
@@ -52,6 +55,7 @@ include '../backend/relatorios.php';
           width: '70%'
         },
         height: 400,
+        width: 900,
         hAxis: {
           minValue: 0,
           textStyle: {

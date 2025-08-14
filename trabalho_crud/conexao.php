8f32b846616013ee49fd2f74fb0fe2b2fa4595e2
Mateus Marcelino <mateusmarcelino1023@gmail.com>
2025-08-14 17:59:53 -0300

    para tcc windows

--- trabalho_crud/conexao.php
+++ trabalho_crud/conexao.php
@@ -1,19 +1,12 @@
 <?php
-
-// Inclui o arquivo que contém a função para carregar o .env
-require_once 'load_env.php';
-
-// Carrega as variáveis de ambiente a partir do arquivo .env
-loadEnv(__DIR__ . '/.env');
-
 // Configura o MySQLi para lançar exceções em caso de erro, facilitando o tratamento
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 // Obtém as variáveis de ambiente para conexão com o banco de dados
-$host = $_ENV['HOST'];       // Endereço do servidor de banco de dados
-$usuario = $_ENV['USUARIO']; // Nome de usuário do banco
-$senha = $_ENV['SENHA'];     // Senha do banco
-$banco = $_ENV['BANCO'];     // Nome do banco de dados
+$host = 'localhost';       // Endereço do servidor de banco de dados
+$usuario = 'host'; // Nome de usuário do banco
+$senha = '';     // Senha do banco
+$banco = 'crud_db';     // Nome do banco de dados
 
 try {
     // Tenta estabelecer a conexão com o banco de dados
